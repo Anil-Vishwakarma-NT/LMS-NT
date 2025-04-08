cd3cd9f20143a506f4fe103030df409ef87aef68
Anil Vishwakarma <anil.vishwakarma@nucleusteq.com>
2025-04-08 15:43:12 +0530

    added logger

--- LMS/src/main/java/com/nt/LMS/controller/AdminController.java
+++ LMS/src/main/java/com/nt/LMS/controller/AdminController.java
@@ -3,15 +3,14 @@ package com.nt.LMS.controller;
 import com.nt.LMS.dto.MessageOutDto;
 import com.nt.LMS.dto.RegisterDto;
 import com.nt.LMS.service.AdminService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
@@ -22,7 +21,11 @@ public class AdminController {
     @PostMapping("/register")
     @PreAuthorize("hasAuthority('admin')")
     public ResponseEntity<MessageOutDto> register(@RequestBody RegisterDto registerDto) {
+        log.info("Admin registration request received for: {}", registerDto.getEmail());
+
         MessageOutDto response = adminService.register(registerDto);
+
+        log.info("Admin registered successfully: {}", registerDto.getEmail());
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 }
--- LMS/src/main/java/com/nt/LMS/service/AdminService.java
+++ LMS/src/main/java/com/nt/LMS/service/AdminService.java
@@ -8,6 +8,7 @@ import com.nt.LMS.entities.User;
 import com.nt.LMS.exception.ResourceConflictException;
 import com.nt.LMS.repository.RoleRepository;
 import com.nt.LMS.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,26 +16,33 @@ import org.springframework.stereotype.Service;
 import java.util.Date;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class AdminService {
 
-     @Autowired
-     private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-     @Autowired
-     private RoleRepository roleRepository;
+    @Autowired
+    private RoleRepository roleRepository;
 
-     @Autowired
-     private PasswordEncoder passwordEncoder;
-
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public MessageOutDto register(RegisterDto registerDto) {
-        Optional<User> ExistingUser = userRepository.findByEmail(registerDto.getEmail());
-        if (ExistingUser.isPresent()) {
+        log.info("Attempting to register user with email: {}", registerDto.getEmail());
+
+        Optional<User> existingUser = userRepository.findByEmail(registerDto.getEmail());
+        if (existingUser.isPresent()) {
+            log.warn("Registration failed - user with email {} already exists", registerDto.getEmail());
             throw new ResourceConflictException(UserConstants.USER_ALREADY_EXISTS);
         }
 
-        Role role = roleRepository.findById(registerDto.getRoleId()).orElseThrow(()-> new RuntimeException("Role does not exist"));
+        Role role = roleRepository.findById(registerDto.getRoleId())
+                .orElseThrow(() -> {
+                    log.error("Role with ID {} not found", registerDto.getRoleId());
+                    return new RuntimeException("Role does not exist");
+                });
 
         User user = new User();
         user.setFirstName(registerDto.getFirstName());
@@ -47,7 +55,8 @@ public class AdminService {
         user.setUpdatedAt(new Date());
 
         userRepository.save(user);
+
+        log.info("User registered successfully with email: {}", registerDto.getEmail());
         return new MessageOutDto(UserConstants.USER_REGISTRATION_SUCCESS);
     }
-
 }
