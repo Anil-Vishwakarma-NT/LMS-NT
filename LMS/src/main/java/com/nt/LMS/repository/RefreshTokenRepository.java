f956417b3c40f192d46bdf39f2f6240db7d63b37
Anil Vishwakarma <anil.vishwakarma@nucleusteq.com>
2025-04-09 11:01:01 +0530

    some changes in refresh token repo

--- LMS/src/main/java/com/nt/LMS/repository/RefreshTokenRepository.java
+++ LMS/src/main/java/com/nt/LMS/repository/RefreshTokenRepository.java
@@ -2,7 +2,11 @@ package com.nt.LMS.repository;
 
 import com.nt.LMS.entities.RefreshToken;
 import com.nt.LMS.entities.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +14,9 @@ import java.util.Optional;
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByToken(String token);
-    void deleteByUser(User user);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM RefreshToken rt WHERE rt.user = :user")
+    void deleteByUser(@Param("user") User user);
 }
