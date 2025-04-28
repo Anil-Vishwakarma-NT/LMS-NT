5b6cfd9cb8e56d96dee2811fbce09082c63afc6d
Sakshi-NT <sakshipurankar88@gmail.com>
2025-04-28 14:31:32 +0530

    admin chnges abort

--- LMS/src/main/java/com/nt/LMS/serviceImpl/AuthServiceImpl.java
+++ LMS/src/main/java/com/nt/LMS/serviceImpl/AuthServiceImpl.java
@@ -80,7 +80,7 @@ public class AuthServiceImpl implements AuthService {
         log.info("Attempting login for email: {}", loginDto.getEmail());
 
         if (loginDto.getEmail() == null || loginDto.getPassword() == null) {
-            log.warn("Login failed - email or password is null");
+            log.warn("Login failed - email or password is null ***************");
             throw new InvalidRequestException(UserConstants.EMAIL_NOT_NULL_MESSAGE);
         }
 
@@ -92,7 +92,7 @@ public class AuthServiceImpl implements AuthService {
                     )
             );
         } catch (Exception ex) {
-            log.error("Authentication failed for email: {}", loginDto.getEmail());
+            log.error("Authentication failed for email: {} ***************", loginDto.getEmail());
             throw new InvalidRequestException(UserConstants.INVALID_CREDENTIALS+" "+ex);
         }
 
