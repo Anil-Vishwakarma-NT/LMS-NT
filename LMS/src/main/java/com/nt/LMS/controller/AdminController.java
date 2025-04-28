f6be664bd29dbb2e6bd67e56ff7525d7d0056c60
Sakshi-NT <sakshipurankar88@gmail.com>
2025-04-28 12:06:32 +0530

    course changes

--- LMS/src/main/java/com/nt/LMS/controller/AdminController.java
+++ LMS/src/main/java/com/nt/LMS/controller/AdminController.java
@@ -1,9 +1,6 @@
 package com.nt.LMS.controller;
 
-import com.nt.LMS.dto.RegisterDto;
-import com.nt.LMS.dto.MessageOutDto;
-import com.nt.LMS.dto.UserOutDTO;
-import com.nt.LMS.dto.UserInDTO;
+import com.nt.LMS.dto.*;
 import com.nt.LMS.serviceImpl.GroupServiceImpl;
 import com.nt.LMS.serviceImpl.UserServiceImpl;
 import com.nt.LMS.serviceImpl.AdminServiceImpl;
@@ -13,13 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 /**
