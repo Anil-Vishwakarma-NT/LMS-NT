897d916d92cab3494e01a52b863b4361d62379d0
Akash Tiwari <akash.tiwari@nucleusteq.com>
2025-05-01 11:58:03 +0530

    minor changes

--- course-service-lms/src/main/java/com/example/course_service_lms/controller/CourseController.java
+++ course-service-lms/src/main/java/com/example/course_service_lms/controller/CourseController.java
@@ -11,14 +11,7 @@ import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -29,6 +22,7 @@ import java.util.Optional;
 @Slf4j
 @RestController
 @RequestMapping("/api/course")
+@CrossOrigin("http://localhost:3000")
 public class CourseController {
 
     /**
