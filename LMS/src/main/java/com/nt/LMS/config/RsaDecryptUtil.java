207a67a93f1282dde3d8db11107e5cea5a7f339d
Sakshi-NT <sakshipurankar88@gmail.com>
2025-06-12 16:10:52 +0530

    rsa key added

---
+++ LMS/src/main/java/com/nt/LMS/config/RsaDecryptUtil.java
@@ -0,0 +1,49 @@
+package com.nt.LMS.config;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.crypto.Cipher;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
+
+public class RsaDecryptUtil {
+
+
+    private  final String privateKey;
+
+    public RsaDecryptUtil(@Value("${private.key}") String privateKey) {
+        this.privateKey = privateKey;
+    }
+
+//public RsaDecryptUtil(@Value("${private.key}") String privateKey) {
+//    this.PRIVATE_KEY = privateKey;
+//}
+//
+
+    public  String decrypt(String encryptedText) {
+        try {
+            // Remove header/footer and decode base64
+            String privateKeyPEM = privateKey
+                    .replace("-----BEGIN PRIVATE KEY-----", "")
+                    .replace("-----END PRIVATE KEY-----", "")
+                    .replaceAll("\\s+", "");
+
+            byte[] keyBytes = Base64.getDecoder().decode(privateKeyPEM);
+            PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            PrivateKey privateKey = kf.generatePrivate(spec);
+
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding"); // Must match frontend padding
+            cipher.init(Cipher.DECRYPT_MODE, privateKey);
+
+            byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
+            return new String(decryptedBytes, StandardCharsets.UTF_8);
+
+        } catch (Exception e) {
+            throw new RuntimeException("RSA decryption failed", e);
+        }
+    }
+}
