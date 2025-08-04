d956e562ad8de0208178a42fe363599ec1d8c568
Aditya <aditya.verma@nucleusteq.com>
2025-08-04 16:49:46 +0530

    Misc files

--- user-service-lms/src/main/java/com/nt/user_service_lms/dto/outDTO/BundleCoursesResponseDTO.java
+++ user-service-lms/src/main/java/com/nt/user_service_lms/dto/outDTO/BundleCoursesResponseDTO.java
@@ -1,13 +1,13 @@
-package com.nt.user_service_lms.dto.outDTO;
-
-import lombok.Data;
-
-import java.util.List;
-
-@Data
-public class BundleCoursesResponseDTO {
-    private Long bundleId;
-    private String bundleName;
-    private List<CourseMetaDTO> courses;
-}
-
+//package com.nt.user_service_lms.dto.outDTO;
+//
+//import lombok.Data;
+//
+//import java.util.List;
+//
+//@Data
+//public class BundleCoursesResponseDTO {
+//    private Long bundleId;
+//    private String bundleName;
+//    private List<CourseMetaDTO> courses;
+//}
+//
