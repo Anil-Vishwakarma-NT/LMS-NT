231671b7e55b729ecc73f57089826e1f54e97740
Anil Vishwakarma <anil.vishwakarma@nucleusteq.com>
2025-08-08 11:12:36 +0530

    s3 feature with upload delete and streaming

--- user-service-lms/src/main/java/com/nt/user_service_lms/service/serviceImpl/AdminServiceImpl.java
+++ user-service-lms/src/main/java/com/nt/user_service_lms/service/serviceImpl/AdminServiceImpl.java
@@ -30,9 +30,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-import static com.nt.user_service_lms.constants.UserConstants.INVALID_REQUEST;
-import static com.nt.user_service_lms.constants.UserConstants.USER_NOT_FOUND;
-import static com.nt.user_service_lms.constants.UserConstants.USER_UPDATED_SUCCESSFULLY;
+import static com.nt.user_service_lms.constants.UserConstants.*;
 
 /**
  * Service implementation for admin operations such as user registration, deletion, role management, and user retrieval.
@@ -112,7 +110,7 @@ public class AdminServiceImpl implements AdminService {
         user.setManagerId(UserConstants.getAdminId());
         userRepository.save(user);
         log.info("User registered successfully with email: {}", registerDto.getEmail());
-        MessageOutDTO messageOutDto = new MessageOutDTO(UserConstants.USER_REGISTRATION_SUCCESS);
+        MessageOutDTO messageOutDto = new MessageOutDTO(USER_REGISTRATION_SUCCESS);
         return StandardResponseOutDTO.success(messageOutDto, USER_REGISTRATION_SUCCESS);
     }
 
